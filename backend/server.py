357726af8c4291d367645fe2bd8cfe91328dc012
uma-manikanta <ikkurthiumamanikanta@gmail.com>
2025-08-28 03:38:48 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,7 @@ origins = [
     "http://localhost:5174",
      "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://bb84-qkd-simulation-blochbusters.vercel.app/"
 ]
 
 app.add_middleware(
